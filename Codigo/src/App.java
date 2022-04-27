60f46e1e715e1966bef96cef768b42d219796301
wildemberg-sales <junior_sales2010@hotmail.com>
2022-04-27 15:39:45 -0300

    ok

--- Codigo/src/App.java
+++ Codigo/src/App.java
@@ -1,5 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String op;
+        
+        
+
     }
+
+    //Menu principal
+    public static String Menu(){
+        String op;
+        op = JOptionPane.showInputDialog("=====Menu Principal=====\nA) Gerenciar Locatarios\nB)Gerenciar Frota\nC)Gerenciar Reservas\nD)Sair do Programa").toUpperCase();
+        return op;
+    }
+
+
 }
