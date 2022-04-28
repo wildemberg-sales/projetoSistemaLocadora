ad3fcbc09e076c4fc5027b81eb815c2cba0baff7
João Pedro <jpmorbeck45@gmail.com>
2022-04-27 22:32:31 -0300

    lista de funcionarios implementada em PessoaJuridica

--- Codigo/src/GerenciadorLocatarios/PessoaJuridica.java
+++ Codigo/src/GerenciadorLocatarios/PessoaJuridica.java
@@ -1,16 +1,20 @@
 package GerenciadorLocatarios;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class PessoaJuridica extends Locatario {
     
     protected String nomeSocial, cnpj;
 
-    //Falta implementar os funcionarios atraves das listas
+    protected List<PessoaFisica> funcionarios;
 
     public PessoaJuridica(String endereco, String bairro, String cidade, String estado, String cep, String email,
             int dddCelular, int numeroCelular, String nomeSocial, String cnpj) {
         super(endereco, bairro, cidade, estado, cep, email, dddCelular, numeroCelular);
         this.nomeSocial = nomeSocial;
         this.cnpj = cnpj;
+        this.funcionarios = new LinkedList<PessoaFisica>();
     }
 
     public String getNomeSocial() {
@@ -29,5 +33,16 @@ public class PessoaJuridica extends Locatario {
         this.cnpj = cnpj;
     }
 
+    public boolean cadastrarFuncionario(PessoaFisica func){
+        boolean resultado = false;
+        for(PessoaFisica t: funcionarios){
+            if(t == func){
+                //Funcionario ja cadastrado
+            } else {
+                resultado = funcionarios.add(func);
+            }
+        }
+        return resultado;
+    }
     
 }
