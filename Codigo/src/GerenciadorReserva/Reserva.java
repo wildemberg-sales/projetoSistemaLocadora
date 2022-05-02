03637e3f196653e785672e02a2e0acc8246b828a
João Pedro <jpmorbeck45@gmail.com>
2022-05-02 15:48:27 -0300

    classe de reserva pronta

--- Codigo/src/GerenciadorReserva/Reserva.java
+++ Codigo/src/GerenciadorReserva/Reserva.java
@@ -1,27 +1,49 @@
 package GerenciadorReserva;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import javax.swing.JOptionPane;
+
+import GerenciadorFrota.Veiculo;
+import GerenciadorLocatarios.Locatario;
+import GerenciadorLocatarios.PessoaFisica;
+import GerenciadorLocatarios.PessoaJuridica;
+
 public class Reserva{
 
     private String dataInicio, dataFim;
-    private int numeroReserva, horaInicio, minutoInicio, horaFim, minutoFim, diarias;
-    private float valorTotal, seguroObrigatorio, imposto;
+    private int numeroReserva, horaInicio, minutoInicio, horaFim, minutoFim, diarias, tipoLocatario;
+    private float valorTotal, seguroObrigatorio, imposto, valorSeguroVeiculo;
     private boolean seguroVeiculo;
+
+    private Locatario locatario;
+    private PessoaFisica responsavelLoc;
+    private Veiculo veiculo;
    
-    public Reserva(String dataInicio, String dataFim, int numeroReserva, int horaInicio, int minutoInicio, int horaFim,
-            int minutoFim, int diarias, float valorTotal, float seguroObrigatorio, float imposto,
-            boolean seguroVeiculo) {
+    public Reserva(String dataInicio, String dataFim, int numeroReserva,int horaInicio, int minutoInicio, 
+                   int horaFim,int minutoFim, boolean seguroVeiculo,int tipoLocatario,
+                    Locatario locatario, Veiculo veiculo, PessoaFisica responsavelLoc) {
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
         this.numeroReserva = numeroReserva;
         this.horaInicio = horaInicio;
         this.minutoInicio = minutoInicio;
         this.horaFim = horaFim;
-        this.minutoFim = minutoFim;
-        this.diarias = diarias;
-        this.valorTotal = valorTotal;
-        this.seguroObrigatorio = seguroObrigatorio;
-        this.imposto = imposto;
+        this.minutoFim = minutoFim;        
         this.seguroVeiculo = seguroVeiculo;
+        this.locatario = locatario;
+        this.veiculo = veiculo;
+        this.tipoLocatario = tipoLocatario;
+        if(tipoLocatario == JOptionPane.YES_OPTION){
+            this.responsavelLoc = responsavelLoc;
+        } else {
+            PessoaFisica self = (PessoaFisica) locatario;
+            this.responsavelLoc = self;
+        }
+
     }
 
     public String getDataInicio() {
@@ -120,6 +142,70 @@ public class Reserva{
         this.seguroVeiculo = seguroVeiculo;
     }
 
-    
+    public float calcularValor(){
+        LocalDate inicio = LocalDate.parse(dataInicio); //aaaa-mm-dd
+        LocalDate fim = LocalDate.parse(dataFim);
+        LocalTime horaI = LocalTime.of(horaInicio, minutoInicio);
+        LocalTime horaF = LocalTime.of(horaFim, minutoFim);
+
+        LocalDateTime dataInicial = LocalDateTime.of(inicio, horaI);
+        LocalDateTime dataFinal = LocalDateTime.of(fim, horaF);
+        int diarias = (int) ChronoUnit.DAYS.between(dataInicial, dataFinal);
+        this.diarias = diarias;
+        
+        float valorFinal;
+        if(this.tipoLocatario == JOptionPane.YES_OPTION){
+            valorFinal = veiculo.getValorDiariaCNPJ() * diarias;
+        } else {
+            valorFinal = veiculo.getValorDiariaCPF() * diarias;
+        }
+        double a = valorFinal * 0.12;
+        double b = valorFinal * 0.05;
+        double c = valorFinal * 0.08;
+
+        this.seguroObrigatorio = (float) a;
+        this.imposto = (float) b;
+        this.valorSeguroVeiculo = (float) c;
+        
+        if(seguroVeiculo){
+            valorFinal += c;
+        }
+        valorFinal += a;
+        valorFinal += b;
+
+        valorTotal = valorFinal;
+        return valorTotal;
+    }
+
+    public void relatorioReserva(){
+        String resposta;
+        float valorFinal;
+        if(this.tipoLocatario == JOptionPane.YES_OPTION){
+            PessoaJuridica a = (PessoaJuridica) locatario;
+            valorFinal = veiculo.getValorDiariaCNPJ(); 
+            resposta = "Reserva: " + numeroReserva + "\nLocatário: " + a.getNomeSocial() +
+                       "\nResponsável pela Locação: " + responsavelLoc.getNome();
+        } else{
+            PessoaFisica a = (PessoaFisica) locatario;
+            valorFinal = veiculo.getValorDiariaCPF();
+            resposta = "Reserva: " + numeroReserva + "\nLocatário: " + a.getNome() + 
+                       "\nResponsável pela Locação: " + responsavelLoc.getNome();
+        }
+
+        resposta += "\n\n                   Dados da Locação                   " +
+                    "\nInício: " + dataInicio + " - " + horaInicio + ":" + minutoInicio +
+                    "\nFim:    " + dataFim + " - " + horaFim + ":" + minutoFim +
+                    "\nNúmero de diárias: " + diarias + 
+                    "\n\nVeiculo: " + veiculo.getMarca() + " - " + veiculo.getModelo() +
+                    "\nAno de Febricação / Modelo: " + veiculo.getAnoFabricacao() + " / " + veiculo.getAnoModelo() +
+                    "\nRenavam: " + veiculo.getRenavam() +
+                    "\n\nValor Diaria: " + valorFinal +
+                    "\nValor do seguro de terceiros: " + seguroObrigatorio +
+                    "\nValor do seguro próprio: " + valorSeguroVeiculo + 
+                    "\nValor dos impostos: " + imposto +
+                    "\nValor total: " + valorTotal;
+
+        JOptionPane.showMessageDialog(null, resposta);
+    }
 
 }
